d94fb90fc939808175e957a47cdb3b6ac6242fdc
presium575730 <presium575730@gmail.com>
2023-01-13 23:52:17 +0900

    solve an ERROR

--- src/main/java/com/ttt/study/web/HelloController.java
+++ src/main/java/com/ttt/study/web/HelloController.java
@@ -1,13 +1,16 @@
 package com.ttt.study.web;
+
+import com.ttt.study.web.HelloController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.
-        RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String Hello() {
         return "hello";
     }
 }
+
--- src/test/java/com/ttt/study/web/HelloControllerTest.java
+++ src/test/java/com/ttt/study/web/HelloControllerTest.java
@@ -1,27 +1,29 @@
 package com.ttt.study.web;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(HelloController.class)
 public class HelloControllerTest {
-
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws Exception {
+    void hello가_리턴된다() throws Exception {
+        //given
         String hello = "hello";
 
+        //when
         mvc.perform(get("/hello"))
+                //then
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
